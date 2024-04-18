4040c1c8d4d2d8e877e1a32887b9390142e394ad
shamshersingh0 <95402470+shamshersingh0@users.noreply.github.com>
2024-04-18 12:16:18 +0530

    second commit

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>Hello World! || Shamsher singh || Basic React</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+ 
+
+  return (
+    <h1>Learn React with Vite || Shamsher singh</h1>
+  )
+}
+
+export default App
