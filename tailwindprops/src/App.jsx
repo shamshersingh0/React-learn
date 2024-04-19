df14cb4917fd99ba067eea532fe3c0b21604118c
shamshersingh0 <95402470+shamshersingh0@users.noreply.github.com>
2024-04-20 00:38:04 +0530

    Learn about how to inject Tailwind css in react project and props

---
+++ tailwindprops/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from "./components/card"
+
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  // let obj = {
+  //   username:"shami",
+  //   age:21,
+  // }
+
+  return (
+    <>
+      <h1 classNameName='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
+      <Card channel="chai aur code" username="Shami" btnText="click me!" />
+      <Card channel="chai aur code" username="Shamsher singh" btnText="visit me!" />
+
+
+
+    </>
+  )
+}
+
+export default App
