bbf2cd743bd09a2f7397a7ce55dd147f9b4e377e
shamshersingh0 <95402470+shamshersingh0@users.noreply.github.com>
2024-04-22 17:31:11 +0530

    handle null image in api issue

--- news-app/src/App.js
+++ news-app/src/App.js
@@ -5,55 +5,69 @@ import Loader from './component/loader'
 function App() {
 
   const [news, setNews] = useState([])
-  const [loading, setLoading] = useState(false) 
-  const[page, setPage] = useState(1)
+  const [loading, setLoading] = useState(false)
+  const [page, setPage] = useState(1)
 
-  console.log(news.length)
+  const emptyArr = []
 
-  const url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=6d2dbf2274554774bf6ae2acf4a78d1d&page=${page}`
-  
+  const url = `https://newsapi.org/v2/everything?q=tesla&from=2024-03-22&sortBy=publishedAt&apiKey=6d2dbf2274554774bf6ae2acf4a78d1d&page=${page}&pageSize=10`
+
   useEffect(() => {
-    setLoading(true); 
-    fetch(url)
-      .then(response => response.json())
-      .then((data) => {
-        console.log(data);
-        setNews(data.articles);
-        setLoading(false); 
-      })
-      .catch(error => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        const pushDataInEmptyArr = emptyArr.push(...data.articles)
+
+        if (pushDataInEmptyArr) {
+          for (let i = 0; i < emptyArr.length; i++) {
+            if (emptyArr[i].urlToImage == null) {
+              console.log(emptyArr[i] + ' is null')
+              emptyArr[i].urlToImage = "https://images.unsplash.com/photo-1591779051696-1c3fa1469a79?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZnJlZSUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D"
+            }
+          }
+
+          setNews(emptyArr);
+          setLoading(false);
+        }
+
+        // console.log(getLength.length);
+
+      } catch (error) {
         console.error('Error fetching data:', error);
         setLoading(false);
-      });
-  }, [url]); 
+      }
+    };
 
-  const handelNextPage = () => {
-    // console.log("NextPage")
+    fetchData();
+  }, [url, page]);
+
+
+
+  const handleNextPage = () => {
     setPage(page + 1)
   }
 
-  const handelPrevPage = () => {
-    // console.log("PrevPage")
-    setPage(page - 1)
+  const handlePrevPage = () => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
   }
-
-
-  
 
   return (
     <>
       {loading && <Loader />} {/* Render Loader component if loading is true */}
-      {!loading && <Card data={news} />} Render Card component if loading is false
-     <div className="flex justify-around">
-     <button onClick={handelPrevPage} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
-  Prev
-</button>
+      {!loading && <Card data={news} />}
+      <div className="flex justify-around m-10">
+        <button onClick={handlePrevPage} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+          Prev
+        </button>
 
-<button disabled={news.length <=1} onClick={handelNextPage} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
-  Next
-</button>
-     </div>
-
+        <button onClick={handleNextPage} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+          Next
+        </button>
+      </div>
     </>
   );
 }
